cc67ac46c9946cedfc701f2bd227a66000ee1ad5
kauane <kauanesantos.coding@gmail.com>
2023-07-24 14:50:44 -0300

    main

--- src/Page/Main/index.js
+++ src/Page/Main/index.js
@@ -1,11 +1,58 @@
-import './App.css';
+import { useState } from 'react';
+import Card from '../../components/Cards';
+import Footer from '../../components/Footer';
+import Header from '../../components/Header';
+import { musics } from '../../musics';
+import './style.css';
 
-function App() {
+
+function Main() {
+
+
+  const [play, setPlay] = useState(musics[0])
+
+
   return (
-    <div className="App">
 
-    </div>
+
+    <div className="container">
+
+
+
+
+      <Header />
+
+      <h3 className='playlist-title'>The best playlist</h3>
+
+      <div className='covers'>
+        {musics.map((music) => (
+          <Card
+            key={music.id}
+            title={music.title}
+            description={music.description}
+            cover={music.cover}
+            music={music}
+            setPlay={setPlay}
+          >
+          </Card>
+        )
+        )}
+      </div>
+
+
+
+      <Footer
+        setPlay={setPlay}
+        play={play}
+      />
+
+
+
+    </div >
+
   );
+
 }
 
-export default App;
+
+export default Main;
