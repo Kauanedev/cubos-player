897d18863bed11d0dc799dd6a90a7839db730168
kauane <kauanesantos.coding@gmail.com>
2023-07-24 14:50:07 -0300

    index updated

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Main from './Page/Main';
+import './Style/style.css';
 
+ReactDOM.render(
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
+    <Main />
+  </React.StrictMode>,
+
+  document.getElementById('root')
+);
